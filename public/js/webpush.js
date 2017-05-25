18684f5ef75ed2b8be56adc4110bcafc181d557e
Just Burrow <just.burrow@lul.kr>
2017-05-26 00:52:06 +0900

    (ADD) 웹 푸시 설정

---
+++ public/js/webpush.js
@@ -0,0 +1,12 @@
+/**
+ * Created by justburrow on 2017. 5. 26..
+ */
+var config = {
+  apiKey: "AIzaSyD0TCtBpbIw_FJ9v4lSMmrgXtGT9aW4XjM",
+  authDomain: "study-5cff7.firebaseapp.com",
+  databaseURL: "https://study-5cff7.firebaseio.com",
+  projectId: "study-5cff7",
+  storageBucket: "study-5cff7.appspot.com",
+  messagingSenderId: "349611704121"
+};
+firebase.initializeApp(config);

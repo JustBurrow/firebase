06cb1d5e40cde2f19e91c4ce8602a543fe1a43dd
Just Burrow <just.burrow@lul.kr>
2017-05-27 11:36:55 +0900

    (ADD) 메시지 토큰 확인을 시도, 에러메시지 확인.
    L.37 에러 메시지.
    
    ```json
    {
       "code":"messaging/failed-serviceworker-registration",
       "message":"Messaging: We are unable to register the default service worker. Failed to register a ServiceWorker: A bad HTTP response code (404) was received when fetching the script. (messaging/failed-serviceworker-registration).",
       "browserErrorMessage":"Failed to register a ServiceWorker: A bad HTTP response code (404) was received when fetching the script."
    }
    ``

--- public/js/webpush.js
+++ public/js/webpush.js
@@ -1,5 +1,6 @@
 /**
  * Created by justburrow on 2017. 5. 26..
+ * @see https://firebase.google.com/docs/cloud-messaging/js/client
  */
 var config = {
   apiKey: "AIzaSyD0TCtBpbIw_FJ9v4lSMmrgXtGT9aW4XjM",
@@ -13,3 +14,25 @@ firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 console.log(messaging);
+
+// 1. 현재의 권한 확인.
+messaging.requestPermission()
+    .then(function () {
+      console.log('has permission.');
+    })
+    .catch(function (err) {
+      console.log('no permission. err ' + err);
+    });
+
+// 2. 토큰 확인.
+messaging.getToken()
+    .then(function (currentToken) {
+      if (currentToken) {
+        console.log("has token.", currentToken);
+      } else {
+        console.log("no token");
+      }
+    })
+    .catch(function (err) {
+      console.log("err : %s", JSON.stringify(err));
+    });

18fec62f198ca83b963dcfb7ac2942f308988512
Just Burrow <just.burrow@lul.kr>
2017-05-28 18:26:02 +0900

    (ADD) 웹 푸시 토큰을 서버로 전송하기.
    푸시 토큰을 서버로 전송해서 서버가 푸시 메시지를 전송할 수 있도록 한다.

--- public/js/webpush.js
+++ public/js/webpush.js
@@ -10,43 +10,59 @@ var config = {
   storageBucket: "study-5cff7.appspot.com",
   messagingSenderId: "349611704121"
 };
-firebase.initializeApp(config);;
-
+firebase.initializeApp(config);
 const messaging = firebase.messaging();
-console.log(messaging);
 
 // 1. 현재의 권한 확인.
-messaging.requestPermission()
-    .then(function () {
-      console.log('has permission.');
-
-    })
-    .catch(function (err) {
-      console.log('no permission. err ' + err);
-    });
+messaging.requestPermission().then(function () {
+  console.log('has permission.');
+}).catch(function (err) {
+  console.log('no permission. err ' + err);
+});
 
 // 2. 토큰 확인.
-messaging.getToken()
-    .then(function (currentToken) {
-      if (currentToken) {
-        console.log("has token.", currentToken);
-      } else {
-        console.log("no token");
-      }
-    })
-    .catch(function (err) {
-      console.log("get token err : %s", JSON.stringify(err));
-    });
+messaging.getToken().then(function (currentToken) {
+  if (currentToken) {
+    console.log("has token.");
+    processValidateAndRegist(currentToken);
+  } else {
+    console.log("no token");
+  }
+}).catch(function (err) {
+  console.log("get token err : %s", JSON.stringify(err));
+});
 
 // 3. 토큰이 변경될 때 갱신하기.
 messaging.onTokenRefresh(function () {
-  messaging.getToken()
-      .then(function (currentToken) {
-        if (currentToken) {
-          console.log("token refreshed.", currentToken);
-        }
-      })
-      .catch(function (err) {
-        console.log("token refresh err : %s", JSON.stringify(err));
-      });
+  messaging.getToken().then(function (currentToken) {
+    if (currentToken) {
+      console.log("token refreshed.");
+      processValidateAndRegist(currentToken);
+    }
+  }).catch(function (err) {
+    console.log("token refresh err : %s", JSON.stringify(err));
+  });
 });
+
+/**
+ * 푸시 토큰을 검증하고 서버에 등록한다.
+ *
+ * @param token
+ */
+function processValidateAndRegist(token) {
+  if ( ! token) {
+    return;
+  }
+  console.log("token : %s", token);
+
+  var data = {
+    'url': $(location).attr('href'),
+    'token': token
+  };
+
+  $.post("/registry/push", data).done(function (resp) {
+    console.log("push token registered", resp);
+  }).fail(function (resp) {
+    console.log("fail to regist push token.", resp);
+  });
+}
